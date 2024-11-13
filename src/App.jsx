9702a6bbbbb3090046344947048f1ac0900f3ffb
sahar <sahar.abdollahi@torontomu.ca>
2024-11-12 22:26:31 -0500

    build WarehouseDetails component folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.scss'
+import WarehouseDetails from './assets/components/WarehouseDetails/WarehouseDetails'
 
 function App() {
   
 
   return (
     <>
-      
+      <WarehouseDetails/>
     </>
   )
 }
