e9695fd7071a053625a9d2d85b6365d5e64e2f23
sbudhia <sanbud@LEGIONOVO.>
2024-11-14 14:02:26 -0600

    Adding warehouse-related pathing to App.jsx to correspond with WarehouseList.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import './App.scss';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import WarehouseList from './components/WarehouseList/WarehouseList.jsx';
 import EditWarehouse from './components/EditWarehouse/EditWarehouse.jsx';
 
 
@@ -9,8 +10,11 @@ function App() {
   return (
     <Router>
       <Routes>
-      <Route path="/" element={<EditWarehouse />} />
-      
+      <Route path="/" element={<WarehouseList />} />
+      <Route path="/warehouses/edit/:id" element={<EditWarehouse/>}/>
+      <Route path="/warehouses/:id" element={<p>WarehouseDetails</p>}/>
+      <Route path="/warehouses/add" element={<p>AddWarehouse</p>}/>
+      <Route path="*" element={<WarehouseList/>}/>      
       </Routes>
     </Router>
   );
