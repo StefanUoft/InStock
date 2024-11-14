4666f545f1b080f07de5e2385da23a4fb22de63d
sahar <sahar.abdollahi@torontomu.ca>
2024-11-14 00:23:31 -0500

    add warehouse details component for  mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.scss'
+import WarehouseDetails from "./components/WarehouseDetails/WarehouseDetails";
 
 function App() {
   
 
   return (
     <>
-      
+      <WarehouseDetails/>
     </>
   )
 }
---
+++ src/components/WarehouseDetails/WarehouseDetails.jsx
@@ -0,0 +1,49 @@
+import "./WarehouseDetails.scss";
+import arrowBackIcon from "../../assets/icons/arrow_back-24px.svg";
+import editIcon from "/src/assets/icons/edit-white-24px.svg";
+
+function WarehouseDetails() {
+  const warehouseData = {
+    name: "Washington",
+    address: "33 Pearl Street SW, Washington, USA",
+    managerName: "Graeme Lyon",
+    contactNumber: "+1 (647) 504-0911",
+    contactEmail: "glyon@instock.com",
+  };
+
+  return (
+    <div className="warehouse">
+      <section className="warehouse__details">
+        <img
+          src={arrowBackIcon}
+          alt="arrow-back"
+          className="warehouse__details-arrow"
+        />
+        <p className="warehouse__details-name">{warehouseData.name}</p>
+        <span className="warehouse__details-icon-span"><img
+          src={editIcon}
+          alt="edit-icon"
+          className="warehouse__details-icon"
+        /></span>
+      </section>
+      <section className="warehouse__info">
+        <div className="warehouse__info-wrapper warehouse__info-wrapper--address">
+          <h4 className="warehouse__info-title">WAREHOUSE ADDRESS:</h4>
+          <p className="warehouse__info-value"> {warehouseData.address}</p>
+        </div>
+        <div className="warehouse__info-wrapper">
+          <h4 className="warehouse__info-title">CONTACT NAME:</h4>
+          <p className="warehouse__info-value"> {warehouseData.managerName}</p>
+          <p className="warehouse__info-position">Warehouse Manager</p>
+        </div>
+        <div className="warehouse__info-wrapper">
+          <h4 className="warehouse_info-title">CONTACT INFORMATION:</h4>
+          <p className="warehouse__info-value">{warehouseData.contactNumber}</p>
+          <p className="warehouse__info-value">{warehouseData.contactEmail}</p>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default WarehouseDetails; 
