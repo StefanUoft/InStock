8fe5055b79e13d20d0c943e3d380372f22263790
sahar <sahar.abdollahi@torontomu.ca>
2024-11-14 16:55:24 -0500

    fix routes :bug:

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import './App.scss'
 import WarehouseDetails from "./components/WarehouseDetails/WarehouseDetails";
+import Header from "./components/Header/Header"
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   
 
   return (
     <>
-      <WarehouseDetails/>
+      <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path="/warehouses/:id" element={<WarehouseDetails/>}/>
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
