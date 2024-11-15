f90ba99ea6f846aa9134ea79503d8638725ccf75
sahar <sahar.abdollahi@torontomu.ca>
2024-11-14 19:57:32 -0500

    add functionality :tada:

--- src/components/WarehouseDetails/WarehouseDetails.jsx
+++ src/components/WarehouseDetails/WarehouseDetails.jsx
@@ -1,29 +1,48 @@
 import "./WarehouseDetails.scss";
 import arrowBackIcon from "../../assets/icons/arrow_back-24px.svg";
 import editIcon from "/src/assets/icons/edit-white-24px.svg";
-import { Link } from "react-router-dom";
+import { Link,useParams } from "react-router-dom";
+import { useState, useEffect } from 'react';
+import axios from "axios";
 
 function WarehouseDetails() {
-  const warehouseData = {
-    name: "Washington",
+  const apiUrl = import.meta.env.VITE_API_URL;
+  const [warehouseData, setWarehouseData] = useState({
+    id:1,
+    warehouse_name: "Washington",
     address: "33 Pearl Street SW, Washington, USA",
-    managerName: "Graeme Lyon",
-    contactNumber: "+1 (647) 504-0911",
-    contactEmail: "glyon@instock.com",
-  };
+    contact_position: "Graeme Lyon",
+    contact_phone: "+1 (647) 504-0911",
+    contact_email: "glyon@instock.com",
+  });
+  const { id } = useParams();
+
+  useEffect(() => {
+    async function getWarehouseData() {
+      try {
+        const response = await axios.get(`${apiUrl}/api/warehouses/${id}`);
+        setWarehouseData(response.data);
+      } catch (error) {
+        console.error("Failed to fetch warehouse data:", error);
+      }
+    }
+
+    getWarehouseData();
+  }, [apiUrl, id]);
+console.log(warehouseData)
 
   return (
     <div className="warehouse">
       <section className="warehouse__details">
-        <Link to={"/"}>
+        <Link to={`/`}>
         <img
           src={arrowBackIcon}
           alt="arrow-back"
           className="warehouse__details-arrow"
         />
         </Link>
-        <p className="warehouse__details-name">{warehouseData.name}</p>
-        <Link to={`/warehouses`} className="warehouse__details-icon">
+        <p className="warehouse__details-name">{warehouseData.warehouse_name}</p>
+        <Link to={`/warehouses/${warehouseData.id}/edit`} className="warehouse__details-icon">
           <img
             src={editIcon}
             alt="edit-icon"
@@ -39,13 +58,13 @@ function WarehouseDetails() {
         </div>
         <div className="warehouse__info-wrapper warehouse__info-wrapper--name">
           <h4 className="warehouse__info-title">CONTACT NAME:</h4>
-          <p className="warehouse__info-value"> {warehouseData.managerName}</p>
+          <p className="warehouse__info-value"> {warehouseData.contact_position}</p>
           <p className="warehouse__info-position">Warehouse Manager</p>
         </div>
         <div className="warehouse__info-wrapper warehouse__info-wrapper--contact">
           <h4 className="warehouse_info-title">CONTACT INFORMATION:</h4>
-          <p className="warehouse__info-value">{warehouseData.contactNumber}</p>
-          <p className="warehouse__info-value">{warehouseData.contactEmail}</p>
+          <p className="warehouse__info-value">{warehouseData.contact_phone}</p>
+          <p className="warehouse__info-value">{warehouseData.contact_email}</p>
         </div>
       </section>
     </div>
