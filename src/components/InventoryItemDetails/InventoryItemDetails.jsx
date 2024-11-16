5a41d51aefd407691e14430228c61e2a8e92ba2e
sbudhia <sanbud@LEGIONOVO.>
2024-11-16 00:40:38 -0600

    Initializing InventoryItemDetails component with mobile styling

---
+++ src/components/InventoryItemDetails/InventoryItemDetails.jsx
@@ -0,0 +1,94 @@
+import "./InventoryItemDetails.scss";
+import arrowBackIcon from "../../assets/Icons/arrow_back-24px.svg";
+import editIcon from "/src/assets/icons/edit-white-24px.svg";
+import { Link, useParams, useNavigate } from "react-router-dom";
+import { useState, useEffect } from 'react';
+import axios from "axios";
+
+
+function InventoryItemDetails() {
+  const apiUrl = import.meta.env.VITE_API_URL;
+  const [itemData, setItemData] = useState(null);
+  const { id } = useParams();
+  const navigate = useNavigate();
+  useEffect(() => {
+    async function getItemData() {
+      try {
+        const response = await axios.get(`http://${apiUrl}/api/inventories/${id}`);
+        setItemData(response.data);
+      } catch (error) {
+        console.error("Failed to fetch item data:", error);
+      }
+    }
+
+    getItemData();
+  }, [apiUrl, id]);
+  console.log(itemData)
+
+  //conditional class for styling
+  const stockStatus = (status) => {
+    return `item-details__info-title ${(status === 'Out of Stock') ? 'out-of-stock' : 'in-stock'}`
+  }
+
+
+  return (
+
+    <>
+      {itemData ? (<div className="item-details">
+
+        <section className="item-details__banner">
+          <div className="item-details__banner__left">
+            <img
+              src={arrowBackIcon}
+              alt="Back Button"
+              className="item-details__banner__back-arrow" onClick={() => navigate(-1)}
+            />
+
+            <h1 className="item-details__banner__name"> {itemData.item_name}</h1>
+          </div>
+          <div className="item-details__banner__right">
+            <Link to={`/inventory/edit/${itemData.id}`} className="item-details__banner__edit-icon">
+              <img
+                src={editIcon}
+                alt="edit-icon"
+                className="item-details__banner__edit-icon-image"
+              />
+              <span className="item-details__banner__edit-icon-text">Edit</span>
+            </Link>
+          </div>
+        </section>
+        <section className="item-details__info">
+          <div className="item-details__info-wrapper item-details__info-wrapper--description">
+            <h4 className="item-details__info-title">ITEM DESCRIPTION:</h4>
+            <p className="item-details__info-value"> {itemData.description}</p>
+          </div>
+          <div className="item-details__info-wrapper item-details__info-wrapper--category">
+            <h4 className="item-details__info-title">CATEGORY:</h4>
+            <p className="item-details__info-value"> {itemData.category}</p>
+          </div>
+          <div className="item-details__info-wrapper item-details__info-wrapper--availability">
+            <div className="item-details__info-wrapper--status">
+              <h4 className={stockStatus(itemData.status)}>STATUS:</h4>
+              <p className="item-details__info-value"> {itemData.status}</p>
+            </div>
+            <div className="item-details__info-wrapper--quantity">
+              <h4 className="item-details__info-title">QUANTITY:</h4>
+              <p className="item-details__info-value"> {itemData.quantity}</p>
+            </div>
+          </div>
+          <div className="item-details__info-wrapper item-details__info-wrapper--contact">
+            <h4 className="item_info-title">WAREHOUSE:</h4>
+            <p className="item-details__info-value">{itemData.warehouse_name}</p>
+          </div>
+        </section>
+
+      </div>) : <h2>Loading</h2>
+      }
+
+    </>
+
+  );
+}
+
+export default InventoryItemDetails;
+
