95ab5bc393a9cafac5546bb2db5cf3c8395175af
sbudhia <sanbud@LEGIONOVO.>
2024-11-17 00:21:27 -0600

    Applying full responsive styling for WarehouseList

--- src/components/WarehouseList/WarehouseList.jsx
+++ src/components/WarehouseList/WarehouseList.jsx
@@ -52,7 +52,7 @@ function WarehouseList() {
     };
 
     const deleteButton = (name) => {
-        return <img src={deleteIcon} onClick={() => openModal(name)} alt="Delete icon" />
+        return <img className="warehouse-list__table__actions__delete" src={deleteIcon} onClick={() => openModal(name)} alt="Delete icon" />
     }
 
     const cardView =
@@ -60,19 +60,19 @@ function WarehouseList() {
             {warehouses.map((wh) =>
                 <div className="warehouse-list__card" key={wh.id} id={wh.id}>
                     <div className="warehouse-list__card__item">
-                        <label className="table_header">WAREHOUSE</label>
-                        <Link className="warehouse-list__card__item__link" to={`/warehouses/${wh.id}`}><h3>{wh.warehouse_name}</h3><img src={chevronRight} /></Link>
+                        <label className="warehouse-list__card__header"><h4>WAREHOUSE</h4></label>
+                        <Link className="warehouse-list__link" to={`/warehouses/${wh.id}`}><h3>{wh.warehouse_name}</h3><img src={chevronRight} /></Link>
                     </div>
                     <div className="warehouse-list__card__item warehouse-list__card__item--right">
-                        <label className="table_header">CONTACT NAME</label>
+                        <label className="warehouse-list__card__header"><h4>CONTACT NAME</h4></label>
                         <p className="p2">{wh.contact_name}</p>
                     </div>
                     <div className="warehouse-list__card__item">
-                        <label className="table_header">ADDRESS</label>
+                        <label className="warehouse-list__card__header"><h4>ADDRESS</h4></label>
                         <p className="p2">{wh.address}, {wh.city}, {wh.country}</p>
                     </div>
                     <div className="warehouse-list__card__item warehouse-list__card__item--right">
-                        <label className="table_header">CONTACT INFORMATION</label>
+                        <label className="warehouse-list__card__header"><h4>CONTACT INFORMATION</h4></label>
                         <p className="p2">{wh.contact_phone}</p><br />
                         <p className="p2">{wh.contact_email}</p>
                     </div>
@@ -90,21 +90,21 @@ function WarehouseList() {
 
     const tableView = (
         <div className="warehouse-list__table-view">
-            <table>
+            <table className="warehouse-list__table">
                 <thead>
-                    <tr>
-                        <th className="table_header">WAREHOUSE <img src={sortIcon}/></th>
-                        <th className="table_header">ADDRESS  <img src={sortIcon}/></th>
-                        <th className="table_header">CONTACT NAME  <img src={sortIcon}/></th>
-                        <th className="table_header">CONTACT INFORMATION  <img src={sortIcon}/></th>
-                        <th className="table_header">ACTIONS  <img src={sortIcon}/></th>
+                    <tr className="warehouse-list__table__header-row warehouse-list__table__row">
+                        <th><div className="warehouse-list__table__header warehouse-list__table__header-pad"><h4>WAREHOUSE </h4><img src={sortIcon}/></div></th>
+                        <th><div className="warehouse-list__table__header"><h4>ADDRESS  </h4><img src={sortIcon}/></div></th>
+                        <th><div className="warehouse-list__table__header"><h4>CONTACT NAME  </h4><img src={sortIcon}/></div></th>
+                        <th><div className="warehouse-list__table__header"><h4>CONTACT INFORMATION  </h4><img src={sortIcon}/></div></th>
+                        <th><div className="warehouse-list__table__header warehouse-list__table__right"><h4>ACTIONS  </h4></div></th>
                     </tr>
                 </thead>
                 <tbody>
                     {warehouses.map((wh) => (
-                        <tr key={wh.id} id={wh.id}>
-                            <td>
-                                <Link to={`/warehouses/${wh.id}`}>
+                        <tr className="warehouse-list__table__row" key={wh.id} id={wh.id}>
+                            <td >
+                                <Link className="warehouse-list__link warehouse-list__table__left" to={`/warehouses/${wh.id}`}>
                                     <h3>{wh.warehouse_name}</h3>
                                     <img src={chevronRight} alt="chevron right icon" />
                                 </Link>
@@ -115,11 +115,13 @@ function WarehouseList() {
                                 <p>{wh.contact_phone}</p>
                                 <p>{wh.contact_email}</p>
                             </td>
-                            <td className="warehouse-list__table-actions">
-                                {deleteButton(wh.warehouse_name)}
-                                <Link to={`/warehouses/edit/${wh.id}`}>
-                                    <img src={editIcon} alt="Edit icon" />
-                                </Link>
+                            <td>
+                                <div className="warehouse-list__table__actions">
+                                    {deleteButton(wh.warehouse_name)}
+                                    <Link to={`/warehouses/edit/${wh.id}`}>
+                                        <img src={editIcon} alt="Edit icon" />
+                                    </Link>
+                                </div>
                             </td>
                         </tr>
                     ))}
@@ -132,14 +134,16 @@ function WarehouseList() {
     return (
         <>
         <Header />
-        <div className="warehouse-list">
+        <div className="main-content">
+        {warehouses ?  ( <div className="warehouse-list">
             <section className="warehouse-list__banner">
-                <h1>Warehouses</h1>
-                <form className="search">
+                <div className="warehouse-list__banner__header"><h1>Warehouses</h1></div>
+                <div className="warehouse-list__banner__interact"><form className="search">
                     <input className="search__input" type="text" name="search" placeholder="Search..." />
                     <img className="search__icon" src={searchIcon} alt="magnifying glass" />
                 </form>
-                <Link className="add-warehouse" to="/warehouses/add"><button className="add-warehouse"><h3>+ Add New Warehouse</h3></button></Link>
+                <Link to="/warehouses/add"><button className="warehouse-list__add-warehouse" ><h3>+ Add New Warehouse</h3></button></Link>
+                </div>
             </section>
             {cardView}
             {tableView}
@@ -151,6 +155,8 @@ function WarehouseList() {
                 itemName={selectedWarehouse}
             />
         </div>
+        ) : <></>
+        }</div>
         </>
     )
 }
