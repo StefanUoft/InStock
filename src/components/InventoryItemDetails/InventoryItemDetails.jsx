e2600b9d1e01e08b7c01fc68591e43f3922d15f8
sbudhia <sanbud@LEGIONOVO.>
2024-11-16 21:48:26 -0600

    Adding desktop/tablet styling for InventoryItemDetails

--- src/components/InventoryItemDetails/InventoryItemDetails.jsx
+++ src/components/InventoryItemDetails/InventoryItemDetails.jsx
@@ -4,6 +4,7 @@ import editIcon from "/src/assets/icons/edit-white-24px.svg";
 import { Link, useParams, useNavigate } from "react-router-dom";
 import { useState, useEffect } from 'react';
 import axios from "axios";
+import Header from "/src/components/Header/Header";
 
 
 function InventoryItemDetails() {
@@ -27,13 +28,14 @@ function InventoryItemDetails() {
 
   //conditional class for styling
   const stockStatus = (status) => {
-    return `item-details__info-title ${(status === 'Out of Stock') ? 'out-of-stock' : 'in-stock'}`
+    return `item-details__info-value stock ${(status === 'Out of Stock') ? 'stock--out' : 'stock--in'}`
   }
 
 
   return (
-
     <>
+    <Header/>
+    <div className="main-content">
       {itemData ? (<div className="item-details">
 
         <section className="item-details__banner">
@@ -57,36 +59,41 @@ function InventoryItemDetails() {
             </Link>
           </div>
         </section>
+
         <section className="item-details__info">
-          <div className="item-details__info-wrapper item-details__info-wrapper--description">
-            <h4 className="item-details__info-title">ITEM DESCRIPTION:</h4>
-            <p className="item-details__info-value"> {itemData.description}</p>
-          </div>
-          <div className="item-details__info-wrapper item-details__info-wrapper--category">
-            <h4 className="item-details__info-title">CATEGORY:</h4>
-            <p className="item-details__info-value"> {itemData.category}</p>
-          </div>
-          <div className="item-details__info-wrapper item-details__info-wrapper--availability">
-            <div className="item-details__info-wrapper--status">
-              <h4 className={stockStatus(itemData.status)}>STATUS:</h4>
-              <p className="item-details__info-value"> {itemData.status}</p>
+          <div className="item-details__info__first">
+            <div className="item-details__info-wrapper item-details__info-wrapper--description">
+              <h4 className="item-details__info-title">ITEM DESCRIPTION:</h4>
+              <p className="item-details__info-value"> {itemData.description}</p>
             </div>
-            <div className="item-details__info-wrapper--quantity">
-              <h4 className="item-details__info-title">QUANTITY:</h4>
-              <p className="item-details__info-value"> {itemData.quantity}</p>
+            <div className="item-details__info-wrapper item-details__info-wrapper--category">
+              <h4 className="item-details__info-title">CATEGORY:</h4>
+              <p className="item-details__info-value"> {itemData.category}</p>
             </div>
           </div>
-          <div className="item-details__info-wrapper item-details__info-wrapper--contact">
-            <h4 className="item_info-title">WAREHOUSE:</h4>
-            <p className="item-details__info-value">{itemData.warehouse_name}</p>
+          <div className="item-details__info__second">
+            <div className="item-details__info-wrapper item-details__info-wrapper--availability">
+              <div className="item-details__info-wrapper--status">
+                <h4 className="item-details__info-title">STATUS:</h4>
+                <p className={stockStatus(itemData.status)}> {itemData.status}</p>
+              </div>
+              <div className="item-details__info-wrapper--quantity">
+                <h4 className="item-details__info-title">QUANTITY:</h4>
+                <p className="item-details__info-value"> {itemData.quantity}</p>
+              </div>
+            </div>
+            <div className="item-details__info-wrapper item-details__info-wrapper--contact">
+              <h4 className="item_info-title">WAREHOUSE:</h4>
+              <p className="item-details__info-value">{itemData.warehouse_name}</p>
+            </div>
           </div>
         </section>
 
       </div>) : <h2>Loading</h2>
       }
 
+    </div>
     </>
-
   );
 }
 
